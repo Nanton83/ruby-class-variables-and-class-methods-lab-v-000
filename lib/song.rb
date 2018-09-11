bbdbb0b37de3976fb66387d24765ff06235c5f0d
Nick Romans <nicholas.romans@gmail.com>
2018-09-11 16:58:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,24 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0
-   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-  attr_accessor :name, :artist, :genre
-end 
-
+  @@genres = []
+  @@artists = []
+  
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count += 1
-  @@genres = [] << genre
-  @@artists = [] << artist
+  @@genres << genre
+  @@artists << artist
 end
 
 def self.count
@@ -28,9 +20,6 @@ def self.count
 end 
 
 def self.genres
-  @@genres.collect do |genre|
-    
-  end 
   end  
   
 def self.artists
