b2abc6071220ac48bb6a2435d70e8aa165526a71
Nick Romans <nicholas.romans@gmail.com>
2018-09-11 17:11:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ end
 
 def self.genre_count
   genre_count = {}
-  @@genres.collect do |genre|
+  @@genres.each do |genre|
     if genre_count[genre]
       genre_count[genre] += 1
     else genre_count[genre] = 1
