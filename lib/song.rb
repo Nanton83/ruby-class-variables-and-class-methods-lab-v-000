cadd5ebc88a2b20b1d3136a2bce16cc87ee02524
Nick Romans <nicholas.romans@gmail.com>
2018-09-11 16:41:15 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@count += 1
+  @@genres = []
+  @@artist = []
+end
+
+def self.count
+  @@count
+end 
+
+def self.genres
+  @@genres
+  end  
+  
+def self.artists
+  @@artist
+end 
+
+end 
